c8dd2b317521c5f2926400466c2583b716897728
shkvo <dimashkvo@gmail.com>
2019-04-03 09:27:36 +0300

    Regions CRUD

--- src/controllers/regions.controller.js
+++ src/controllers/regions.controller.js
@@ -34,7 +34,64 @@ const get = async ctx => {
   }
 };
 
+const create = async ctx => {
+  try {
+    const region = ctx.request.body;
+    const data = await Regions.create(region);
+
+    ctx.body = {
+      status: 'success',
+      data
+    };
+  } catch (error) {
+    ctx.body = {
+      message: 'Error has occured',
+      error
+    };
+  }
+};
+
+const update = async ctx => {
+  try {
+    const region = ctx.request.body;
+    const data = await Regions.update(region);
+
+    ctx.body = {
+      status: 'success',
+      data
+    };
+  } catch (error) {
+    ctx.body = {
+      message: 'Error has occured',
+      error
+    };
+  }
+};
+
+const remove = async ctx => {
+  const { id } = ctx.params;
+  const region = await Regions.findOne({
+    where: {
+      id
+    }
+  });
+  try {
+    await region.destroy();
+    ctx.body = {
+      status: 'success',
+    };
+  } catch (error) {
+    ctx.body = {
+      message: 'Error has occured',
+      error
+    };
+  }
+};
+
 export default {
 	all,
-	get
+  get,
+  create,
+  update,
+  remove
 };
