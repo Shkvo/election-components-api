5256361fd67b50b2293c3ce05d421efdee6128dc
shkvo <dimashkvo@gmail.com>
2019-04-28 00:17:20 +0300

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import convert from 'koa-convert';
 import bodyParser from 'koa-bodyparser';
+import cors from 'koa-cors';
 import logger from 'koa-logger';
 import passport from 'koa-passport';
 import helmet from 'koa-helmet';
@@ -8,7 +9,7 @@ import helmet from 'koa-helmet';
 import router from './routes';
 
 const app = new Koa();
-
+app.use(cors());
 require('dotenv').config();
 
 require('./config/auth');
